3fda90bff34652f82d81404e9ccca014888aaa84
katjakowalski <kowalskk@hu-berlin.de>
2018-05-20 20:55:03 +0200

    20180520 complete assignment

--- week05/assignment03.py
+++ week05/assignment03.py
@@ -2,21 +2,10 @@
 # and submit the numbers to moodle. Be aware of potential raster values in the topographic variables that may indicate
 # NoData values but are not flagged as such (e.g., 65536 in the elevation layer).
 
-
-    #In addition, calculate the proportional area (two decimal digits) of raster cells having the value ‘1’ relative to
-    # the entire area. Upload this number to moodle.
-
 import gdal
 import glob
 import os
 import numpy as np
-import numpy.ma as ma
-import gdal
-
-#pr = dem_ras.GetProjection()
-#cols = dem_ras.RasterXSize
-#rows = dem_ras.RasterYSize
-#dem_arr = dem.ReadAsArray(0,0, cols, rows) # origin x, origin y, slice size x, slice size y > get subset in the same way
 
 root_folder = "/Users/Katja/Documents/Studium/Sose18/week05/Assignment03 - data/"
 file_list = glob.glob(os.path.join(root_folder, '*.tif'))
@@ -39,8 +28,8 @@ def rasteroverlap(file_list):
         print(os.path.basename(i), 'Upper Left X:',UL_x, 'Upper Left Y:', UL_y, 'Lower Right X:', LR_x, 'Lower Right Y:', LR_y)
     print('UL X:', max(UL_x_list),'UL Y:', min(UL_y_list))
     print('LR X:', min(LR_x_list),'LR Y:', max(LR_y_list))
-    print('X range',(min(LR_x_list)-max(UL_x_list))/gt[1])  # no of columns in x direction
-    print('Y range', (min(UL_y_list)-max(LR_y_list))/gt[1]) # no of rows in y direction
+    print('X range',(min(LR_x_list)-max(UL_x_list))//gt[1])  # no of columns in x direction
+    print('Y range', (min(UL_y_list)-max(LR_y_list))//gt[1]) # no of rows in y direction
 
 
 rasteroverlap(file_list)
@@ -48,6 +37,7 @@ rasteroverlap(file_list)
 ## DEM
 dem_ras = gdal.Open('/Users/Katja/Documents/Studium/Sose18/week05/Assignment03 - data/DEM_Humboldt_sub.tif')
 gt_dem = dem_ras.GetGeoTransform()
+pr_dem = dem_ras.GetProjection()
 inv_dem = gdal.InvGeoTransform(gt_dem)
 #print(inv_dem)
 
@@ -75,29 +65,68 @@ inv_thp = gdal.InvGeoTransform(gt_thp)
 offsets_thp = gdal.ApplyGeoTransform(inv_thp, 1399618.9749825108, 705060.6257949192)
 xoff3, yoff3 = map(int, offsets_thp)
 arr_thp = thp_ras.ReadAsArray(xoff3, yoff3, 599, 1240)
-#print(arr_thp)
 
-arr_dem = ma.masked_where(arr_dem >= 8000, arr_dem)
-arr_slo = ma.masked_where(arr_slo < 0, arr_slo)
-arr_thp = ma.masked_where(arr_thp > 10000, arr_thp)
-print(' mean DEM:', np.mean(arr_dem),
-      "\n", 'min DEM:', np.min(arr_dem),
-      '\n', 'max DEM:', np.max(arr_dem),
-      '\n', 'mean slope:', np.mean(arr_slo),
-      '\n', 'min slope:', np.min(arr_slo),
-      '\n', 'max slope:', np.max(arr_slo))
+# mask values of different arrays
+arr_dem = np.ma.masked_where(arr_dem >= 8000, arr_dem, copy=True)
+arr_slo = np.ma.masked_where(arr_slo < 0, arr_slo, copy=True)
+arr_thp = np.ma.masked_where(arr_thp > 10000, arr_thp, copy=True)
 
-
+print(' mean DEM:', round(np.mean(arr_dem),2),
+      "\n", 'min DEM:', round(np.min(arr_dem),2),
+      '\n', 'max DEM:', round(np.max(arr_dem),2),
+      '\n', 'mean slope:', round(np.mean(arr_slo),2),
+      '\n', 'min slope:', round(np.min(arr_slo),2),
+      '\n', 'max slope:', round(np.max(arr_slo),2))
 
 
 # Now take the elevation and slope layers only, and build a binary mask in which areas with elevation < 1000m and
 # slope <30deg have the value ‘1’, and all other areas the value ‘0’. Write this binary mask into a new raster file
 # and upload it to moodle.
+# In addition, calculate the proportional area (two decimal digits) of raster cells having the value ‘1’ relative to
+# the entire area. Upload this number to moodle.
+
+arr_B1 = (arr_dem < 1000)                  # creates True/false array
+arr_B2 = (arr_slo < 30)
+arr_B12 = (arr_B2 == 1) & (arr_B1 == 1)     # creates T/F array where other array values are 1
+arr_B12 = arr_B12*1                         # creates 1/0 array
+
+print(arr_B12)
+print("total px: ", np.count_nonzero(arr_B12 != 1)+np.count_nonzero(arr_B12 == 1),'\n' 
+      "px 1: ", np.count_nonzero(arr_B12 == 1),'\n'
+      "px 0: ", np.count_nonzero(arr_B12 == 0), '\n'
+      "% px 1: ", round((np.count_nonzero(arr_B12 == 1)*100)/(np.count_nonzero(arr_B12 != 1)+np.count_nonzero(arr_B12 == 1)),2),'\n'
+      "% px 0: ", (np.count_nonzero(arr_B12 == 0)*100)/(np.count_nonzero(arr_B12 != 1)+np.count_nonzero(arr_B12 == 1)))
+
+# Save raster - code from class
+# 0. Formulate an outputName
+ras_B12 = root_folder + "dem_slope.tif"
+# 1. Create a driver with which we write the output
+drvR = gdal.GetDriverByName('GTiff')
+# 2. Create the file (here: allthough exactly the same, we go through the syntax)
+outDS = drvR.Create(ras_B12, 599, 1240, 1)
+outDS.SetProjection(pr_dem)
+outDS.SetGeoTransform(gt_dem)
+# 3. Write the array into the newly generated file
+outDS.GetRasterBand(1).WriteArray(arr_B12, 0, 0)#(array, offset_x, offset_y)
 
 
-arr_stack = ma.dstack((arr_dem, arr_slo))
 
+#For each of the areas of the different values in the THP raster dataset (i.e., 1997-2015) calculate the mean values
+# slope and elevation with two decimal digits.
+#Write the results into a comma-separated values file, and upload the file to moodle. Each row thereby should have the
+# format Year, Mean_elev, Mean_slope
 
-#GetGeoTransform > have to create a new one for final output file  out_ds.SetProjection(old_projectionvariable) (Projection)
-# /out_ds.SetGeoTransform(new_gt_object) (pixel size)
+yearslist = list(range(1997,2016))
+
+mean_list = list()
+for i in yearslist:
+    mean_list.extend((i, round(np.mean(arr_dem[arr_thp == i]),2)))
+    mean_list.append((round(np.mean(arr_slo[arr_thp == i]),2)))
+
+csv_array = np.array(mean_list)
+csv_array = np.reshape(csv_array, (-1, 3))
+print(csv_array)
+
+np.savetxt('means.csv', csv_array, fmt='%.2f', delimiter=';', header='Year; Mean_elev; Mean_slope')
+
 
