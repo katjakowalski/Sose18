5ade3e128b675260f38068f37e4dac52a3c18c32
katjakowalski <kowalskk@hu-berlin.de>
2018-05-27 19:28:12 +0200

    20180527 final version, csv code missing

--- week06/Assignment04.py
+++ week06/Assignment04.py
@@ -25,6 +25,7 @@ import geopandas as gpd
 root_folder = '/Users/Katja/Documents/Studium/Sose18/week06/'
 os.chdir('/Users/Katja/Documents/Studium/Sose18/week06/')
 
+# load shapefiles
 driver = ogr.GetDriverByName("ESRI Shapefile")
 CO = driver.Open(root_folder + '/GER_SUI_subset.shp')
 PA = driver.Open(root_folder + '/subset_WDPA.shp')
@@ -32,40 +33,33 @@ PA = driver.Open(root_folder + '/subset_WDPA.shp')
 CO_lyr = CO.GetLayer()
 PA_lyr = PA.GetLayer()
 
+
+# select terrestrial and designated and established PAs with SQL
 sql = "SELECT name, iucn_cat, status_yr, gis_area, status  " \
       "FROM subset_WDPA WHERE marine = '0' AND (status = 'Designated' OR 'Established')"
 PA_subset = PA.ExecuteSQL(sql)
 
-# create prj file
-spatialRef = osr.SpatialReference()
-spatialRef.ImportFromEPSG(4326)
-spatialRef.MorphToESRI()
-file = open('PA_subset.prj', 'w')
-file.write(spatialRef.ExportToWkt())
-file.close()
+# load into memory to make changes to the shapefile
+drv = ogr.GetDriverByName( 'Memory' )
+outds = drv.CreateDataSource( '' )
+PA_subset = outds.CopyLayer(PA_subset,'test2')
 
-# write subset to disk
-drv = ogr.GetDriverByName( 'ESRI Shapefile' )
-outds = drv.CreateDataSource( "PA_subset.shp" )
-outlyr = outds.CopyLayer(PA_subset,'PA_subset')
-
-# close shapefile
-PA_subset = None
-del outlyr,outds
-
-# open subset again
-PA_in = driver.Open(root_folder + '/PA_subset.shp')
-PA_subset = PA_in.GetLayer()
-
-# add fields for country and country id
 layer_defn = PA_subset.GetLayerDefn()
+
+# create two new fields (country and country id):
 field_defn = ogr.FieldDefn("country", ogr.OFTString) # define new field for shapefile
-field_defn2 = ogr.FieldDefn("country_id", ogr.OFTString)
+field_defn.SetWidth(20) # set length of string
 PA_subset.CreateField(field_defn)
 
-#get field names
+field_defn2 = ogr.FieldDefn("country_id", ogr.OFTInteger)
+field_defn2.SetWidth(5)
+PA_subset.CreateField(field_defn2)
+
+
+#get field names to check if it worked
 field_names = [layer_defn.GetFieldDefn(i).GetName() for i in range(layer_defn.GetFieldCount())]
 print(field_names)
+
 
 
 # loop through country layer, set spatial filter and then loop through PA layer and set variable to country
@@ -78,26 +72,26 @@ for country in CO_lyr:
     print(co_name, PA_subset.GetFeatureCount())
     for i in PA_subset:
         PA_subset.SetFeature(i)
-        i.SetField("country_id", co_name)       # I dont know if it works to set two fields at once, otherwise the three lines have to be copied for each field
-        i.SetField("country", co_id)
+        i.SetField("country_id", co_id)      # set values for country and country id
+        i.SetField("country", co_name)
         PA_subset.SetFeature(i)
-        PA_df.append([i.GetField('name'), i.GetField('iucn_cat'), i.GetField('status_yr'), i.GetField('gis_area')]) # we need to get country and country id here as well
+        PA_df.append([i.GetField('name'), i.GetField('iucn_cat'),
+                      i.GetField('status_yr'), i.GetField('gis_area'),
+                      i.GetField('country'), i.GetField('country_id')]) # add all variables which we need to list (each 'row' is a list)
     PA_subset.SetSpatialFilter(None)
 
-#print(PA_df)
 
 ## PANDAS DATAFRAME ##
 
-field_names = list(('name', 'iucn_cat',  'status_yr', 'gis_area'))
+# create pandas DF with column names
+field_names = list(('name', 'iucn_cat',  'status_yr', 'gis_area', 'country', 'country_id'))
 PA_dff = pd.DataFrame.from_records(PA_df, columns = field_names)
 
-# change all iucn cat Ia,b,c ... to I
+# change all iucn cat Ia,b,c ... to I:
+PA_dff = PA_dff.replace(to_replace = ['Ia','Ib', 'Ic', 'Id'], value = 'I') # Check if there are more categories like IIa, IIb etc.
 
 
-PA_dff = PA_dff.replace(to_replace = ['Ia','Ib', 'Ic', 'Id'], value = 'I') # check what categories have to replaced as well
-
-
-# category ALL, by country = columns "mean area", 'max area',
+# category ALL, by country = columns "mean area", 'max area'
 all_mean = PA_dff.groupby("status_yr")['gis_area'].mean().reset_index() # mean area; group by country instead of year
 all_max = PA_dff.groupby("status_yr")['gis_area'].max().reset_index() # max area; group by country instead of year
 
@@ -110,7 +104,9 @@ iucn_mean = PA_dff.groupby(['iucn_cat', 'status_yr'])['gis_area'].mean().reset_i
 # number of PA's per country and per IUCN category + country
 iucn_count = PA_dff.groupby(['iucn_cat', 'status_yr']).count().reset_index()
 all_count = PA_dff.groupby(['status_yr']).count().reset_index()
-print(iucn_count)
-print(all_count)
+#print(iucn_count)
+#print(all_count)
 
-PA_dff.loc[PA_dff.reset_index().groupby(['status_yr'])['to_date'].idxmax()]
+# max gis area per country and per iucn cat with all variables
+print(PA_dff.loc[PA_dff.reset_index().groupby(['country'])['gis_area'].idxmax()])
+print(PA_dff.loc[PA_dff.reset_index().groupby(['country', 'iucn_cat'])['gis_area'].idxmax()])
