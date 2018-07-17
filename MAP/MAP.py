0ba445e3d8ae4d485e4463289235e75f3fb943e1
katjakowalski <kowalskk@hu-berlin.de>
2018-07-17 14:27:36 +0200

    17.07.2018

--- MAP/MAP.py
+++ MAP/MAP.py
@@ -46,7 +46,6 @@ def corner_coordinates(file_list):
         UL_y_list.append(UL_y)
         LR_x_list.append(LR_x)
         LR_y_list.append(LR_y)
-        LS_list.append(LS)
         #print(os.path.basename(i), 'Upper Left X:',UL_x, 'Lower Right X:', LR_x)
     x_min = max(UL_x_list)
     x_max = min(LR_x_list)
@@ -56,10 +55,9 @@ def corner_coordinates(file_list):
 
 
 ######################################################
-# Aim: make sure that raster files have the same extent
-## search for all files in subfolders of data
+
+# get all files in subfolders of data
 root_folder = "/Users/Katja/Documents/Studium/Sose18/MAP/Geoprocessing-in-python_MAP2018_data/Task01_data/"
-LS_list = list()            # list all subdirectories with file path and name of subfolders
 file_list = list()
 for root, dirs, files in os.walk(root_folder, topdown=False):
     for name in files:
@@ -83,18 +81,16 @@ print(x_min, x_max, y_min, y_max)
 
 # coordinate transformation
 ras_veg = gdal.Open(root_folder + '/2000_VCF/20S_070W.tif')
-pr_veg = ras_veg.GetProjection()               # get projection from Veg. raster
-#print(pr_veg)
-target_SR = osr.SpatialReference()          # create empty spatial reference
-target_SR.ImportFromWkt(pr_veg)             # get spatial reference from projection of raster
+pr_veg = ras_veg.GetProjection()                            # get projection from Veg. raster
+target_SR = osr.SpatialReference()                          # create empty spatial reference
+target_SR.ImportFromWkt(pr_veg)                             # get spatial reference from projection of raster
 
 # coordinate transformation Landsat/Sentinel data <> Vegetation data
 ras_LS = gdal.Open(root_folder + '/2015_L8_doy015/Tile_x18999_y38999_1000x1000_2014-2015_CHACO_PBC_multiYear_Imagery.bsq')
-pr_LS = ras_LS.GetProjection()               # get projection from Veg. raster
-#print(pr_LS)
-source_SR = osr.SpatialReference()          # create empty spatial reference
-source_SR.ImportFromWkt(pr_LS)             # get spatial reference from projection of raster
-coordTrans = osr.CoordinateTransformation(source_SR, target_SR)     # transformation rule for coordinates from samples to elevation raster
+pr_LS = ras_LS.GetProjection()                              # get projection from Veg. raster
+source_SR = osr.SpatialReference()                          # create empty spatial reference
+source_SR.ImportFromWkt(pr_LS)                              # get spatial reference from projection of raster
+coordTrans = osr.CoordinateTransformation(source_SR, target_SR)     # transformation rule
 
 
 # create random samples in interval (float) and add ID and coordinates to dataframe
@@ -105,50 +101,48 @@ count_list4 = []
 count_list5 = []
 df = pd.DataFrame(columns=['ID', 'X', 'Y'])                         # set up pandas dataframe to store data
 
-while len(count_list1) < 100 and len(count_list2) < 100 and len(count_list3) < 100 and len(count_list4) <100 and len(count_list5)<100:
-    x1 = rd.uniform(x_min, x_max)                                   # random value in x range
-    y1 = rd.uniform(y_min, y_max)                                   # random value in y range
-    ras = gdal.Open(root_folder + '/2000_VCF/20S_070W.tif')
-    rb_ras = ras.GetRasterBand(1)
+while len(count_list1) < 1 or len(count_list2) < 1 or len(count_list3) < 1 or len(count_list4) < 1 or len(count_list5) < 1:
+    x1 = rd.uniform(x_min, x_max)                                   # sample random value in x range
+    y1 = rd.uniform(y_min, y_max)                                   # sample random value in y range
+    ras = gdal.Open(root_folder + '/2000_VCF/20S_070W.tif')         # open vegetation raster
     point = ogr.Geometry(ogr.wkbPoint)                              # create point geometry from coordinates
     point.AddPoint(x1, y1)
-    #print(point)
-    #coord = point.GetGeometryRef()
     coord_cl = point.Clone()                                        # clone sample geometry
     coord_cl.Transform(coordTrans)                                  # apply coordinate transformation
     x, y = coord_cl.GetX(), coord_cl.GetY()                         # get x and y coordinates of transformed sample point
-    print(x,y)
-    struc_ras = rb_ras.ReadRaster(x, y, 1, 1)
+    # calculate absolute raster coordinates:
+    gt_ras = ras.GetGeoTransform()
+    px_ras = int((x - gt_ras[0]) / gt_ras[1])
+    py_ras = int((y - gt_ras[3]) / gt_ras[5])
+    rb_ras = ras.GetRasterBand(1)
+
+    # extract information from raster and unpack values
+    struc_ras = rb_ras.ReadRaster(px_ras, py_ras, 1, 1)
     if struc_ras is None:
         val = struc_ras
     else:
-        val_ras = struct.unpack('H', struc_ras)
+        val_ras = struct.unpack('b', struc_ras)
         val = val_ras[0]
-    print(val)
-    # if val == None:
-    #     print("No Data")
-    # if 0 <= val <= 20:
-    #     count1 = len(count_list1)
-    #     df.loc[len(df) + 1] = [count1, x, y]
-    #     count_list1.append(1)
-    # else: print('coordinate not in stratum 1 ')
-    # if 21 <= val <= 40:
-    #     count2 = len(count_list2)
-    #     df.loc[len(df) + 1] = [count2, x, y]
-    #     count_list2.append(1)
-    # if 41 <= val <= 60:
-    #     count3 = len(count_list3)
-    #     df.loc[len(df) + 1] = [count3, x, y]
-    #     count_list3.append(1)
-    # if 61 <= val <= 80:
-    #     count4 = len(count_list4)
-    #     df.loc[len(df) + 1] = [count4, x, y]
-    #     count_list4.append(1)
-    # if 81 <= val <= 100:
-    #     count5 = len(count_list5)
-    #     df.loc[len(df) + 1] = [count5, x, y]
-    #     count_list1.append(1)
-print(df)
+    if val <= 20 and len(count_list1) < 1:
+        count1 = len(count_list1)
+        df.loc[len(df) + 1] = [count1, x, y]
+        count_list1.append(1)
+    elif val <= 40 and len(count_list2) < 1:
+        count2 = len(count_list2)
+        df.loc[len(df) + 1] = [count2, x, y]
+        count_list2.append(1)
+    elif val <= 60 and len(count_list3) < 1:
+        count3 = len(count_list3)
+        df.loc[len(df) + 1] = [count3, x, y]
+        count_list3.append(1)
+    elif val <= 80 and len(count_list4) < 1:
+        count4 = len(count_list4)
+        df.loc[len(df) + 1] = [count4, x, y]
+        count_list4.append(1)
+    elif val <= 100 and len(count_list5) < 1:
+        count5 = len(count_list5)
+        df.loc[len(df) + 1] = [count5, x, y]
+        count_list5.append(1)
 
 #####################################################################################
 # set ending time ###################################################################
